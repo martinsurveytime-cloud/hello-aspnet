b57fcfb7ed6e9f7ec9fbf61b31ac6c2ec68ccb6a
Martin Rindos <martinsurveytime@gmail.com>
2025-09-04 08:40:52 +0200

    Add files via upload

---
+++ kod/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using static System.Math;
+Console.WriteLine("Hello, World!");
+
+int cislo = 20;
+
+double idk = cislo / 1.0;
+Console.WriteLine(idk);
+
+double jj = 12.5;
+int kk = (int)jj;
+Console.WriteLine(kk);
+
+Console.WriteLine(Math.Round(jj));
+
+Console.WriteLine(Math.Pow(jj, 5));
